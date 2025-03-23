b145b7553cf91d884978f949caf0672388534e65
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-03-23 07:20:00 +0400

    base layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import '../styles/Header.css';
+import { useMediaQuery } from 'react-responsive';
 
 function Header(){
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
     return (
         <div className="Header">
-            Dash Board
+            <h2>Dash Board</h2>
+            {isMobile? <div>Place Holder Mobile</div>: <div>Place Holder Desktop</div>}
         </div>
     );
 }
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,12 @@
+import '../styles/SideBar.css'
+
+function SideBar(){
+    return (
+        <div className="sideBar">
+            <button>Map View</button>
+            <button>Recent Reports</button>
+        </div>
+    );
+}
+
+export default SideBar;
