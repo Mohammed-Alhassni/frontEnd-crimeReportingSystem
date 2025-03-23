5d6f0d39846e5aa4f81114d8ed57e61531f12b6a
mohammed <mohammed@domain.com>
2025-03-23 18:24:26 +0400

    fixed map displaying issue

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
-// Define your locations (latitude, longitude)
-const locations = [
-  { id: 1, lat: 51.505, lng: -0.09, name: 'Location 1' },
-  { id: 2, lat: 51.515, lng: -0.1, name: 'Location 2' },
-  { id: 3, lat: 51.525, lng: -0.11, name: 'Location 3' }
-];
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
 function MapComponent() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ width: '100%', height: '100%' }}>
-      {/* TileLayer to show the map background */}
+    <MapContainer center={[21.4735, 55.9754]} zoom={13}>
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      
-      {/* Map Markers */}
-      {locations.map(location => (
-        <Marker key={location.id} position={[location.lat, location.lng]}>
-          <Popup>{location.name}</Popup>
-        </Marker>
-      ))}
     </MapContainer>
   );
 }
