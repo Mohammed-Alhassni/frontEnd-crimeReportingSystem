ef531f3efb9cda31359e981188d3ddf736ba480c
mohammed <mohammed@domain.com>
2025-03-26 05:20:10 +0400

    add map marker

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
 function MapComponent() {
+  const position = [23.588, 58.3829];
+
   return (
     <MapContainer center={[21.4735, 55.9754]} zoom={7}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={position}>
+        <Popup>
+          This is a marker at the center of the map!
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
