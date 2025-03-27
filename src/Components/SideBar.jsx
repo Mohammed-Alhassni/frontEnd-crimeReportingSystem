02a5fd1833b769da96b7664aee13e3bae2f59b14
mohammed <mohammed@domain.com>
2025-03-27 07:27:08 +0400

    implemented react router

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,12 +1,14 @@
 import '../styles/SideBar.css'
+import { Link } from "react-router-dom";
+
 
 function SideBar(){
     return (
         <div className="sideBar">
-            <button>Reported Crimes</button>
-            <button>Crimes List</button>
-            <button>Report a Crime</button>
-            <button>About</button>
+            <button><Link to="/Map">Reported Crimes</Link></button>
+            <button><Link to="/List">Crimes List</Link></button>
+            <button><Link to="/SubmitReport">Report a Crime</Link></button>
+            <button><Link to="/About">About</Link></button>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import DashBoard from './DashBoard';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DashBoard/>
+    <BrowserRouter>
+      <DashBoard/>
+    </BrowserRouter>
   </StrictMode>,
 )
