cdba1208cc48d5987a2422beace3448320e5a7b9
mohammed <mohammed@domain.com>
2025-03-29 08:41:00 +0400

    deleted crimelist

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -6,7 +6,6 @@ function SideBar(){
     return (
         <div className="sideBar">
             <div><Link id to="/Map">Reported Crimes</Link></div>
-            <div><Link to="/List">Crimes List</Link></div>
             <div><Link to="/ReportSubmission">Report a Crime</Link></div>
             <div><Link to="/About">About</Link></div>
         </div>
